94656253572580df4d88717d50db7df6d7f227b6
TidyMoMan <mosesdilts@gmail.com>
2026-03-06 15:11:09 -0500

    added assignment 3 readme

--- hw2/pa2.c
+++ hw2/pa2.c
@@ -2,14 +2,51 @@
 #include <stdlib.h>
 #include "tree.h"
 
-#define DEBUG 1
+void outputOne(char*, char*);
+void outputTwo(char*, char*);
+void outputThree(char*, char*);
 
-int main(){
+void dumpPost(tree*);
 
-  char* file = "/home/mo/ece368/hw2/pa2_examples/examples/8.pr";
-  printf("testing program on input file: %s\n", file);
+int main(int argn, char** argv){
 
-  tree* root = loadTree(file);
+  //if(argn != 5){return EXIT_FAILURE;} //fail because of too many arguments
+
+  //argv[0] contains the program name
+  char* inputFile = argv[1]; //-> input file
+  char* outFile1 = argv[2]; //-> output file 1
+  char* outFile2 = argv[3]; //-> output file 2
+  char* outFile3 = argv[4]; //-> output file 3
+
+  if(DEBUG)printf("testing program on input file: %s\n", inputFile);
   
-  return 0;
+  //create each output file
+  outputOne(inputFile, outFile1);
+  outputTwo(inputFile, outFile2);
+  outputThree(inputFile, outFile3);
+  
+  return EXIT_SUCCESS;
+}
+
+void outputOne(char* inputFile, char* outputFile){
+  tree* root = loadPreorder(inputFile); //load tree in pre-order
+  writePostorder(root, outputFile); //write to file in post-order
+  freeTree(root); //free tree memory
+}
+
+void outputTwo(char* inputFile, char* outputFile){
+  
+}
+
+void outputThree(char* inputFile, char* outputFile){
+  
+}
+
+//debugging tool to print post order traversal
+void dumpPost(tree* root){
+  if(root == NULL){return;}
+  dumpPost(root->left);
+  dumpPost(root->right);
+  printf("%d\n",root->blockNum);
+  return;
 }
--- hw2/tree.h
+++ hw2/tree.h
@@ -1,3 +1,9 @@
+#define LEAF 'L'
+#define VERT 'V'
+#define HORIZ 'H'
+
+#define DEBUG 1
+
 typedef struct tree_{ //define the binary tree type
   char div;
 
@@ -9,4 +15,9 @@ typedef struct tree_{ //define the binary tree type
   struct tree_* right;
 }tree;
 
-tree* loadTree(char*);
+/*loaders and writers*/
+tree* loadPreorder(char*);
+int writePostorder(tree*, char*);
+
+/*util stuff*/
+int freeTree(tree*);
